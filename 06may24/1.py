e467cc39c505ee550f774fc9633e1f4e146f5adc
malathisankar <110773069+malathisankar@users.noreply.github.com>
2024-05-09 16:30:07 +0530

    Add files via upload

---
+++ 06may24/1.py
@@ -0,0 +1,12 @@
+mark = int(input("Enter your mark : "))
+if mark >= 35 and mark <= 45:
+   print("C grade")
+elif mark >=46 and mark <= 75:
+   print("B grade")
+elif mark >=76 and mark <= 90:
+   print("A grade")
+elif mark >=91 and mark <= 100:
+   print("O grade")
+else:
+   print("fail")
+
---
+++ 06may24/8.py
@@ -0,0 +1,10 @@
+a=0
+amount = 25000
+if amount < 1000 :
+  a = amount - 200
+else:
+  if amount > 1000 :
+     a = amount + 250
+  else :
+     a = 0
+print(a)
